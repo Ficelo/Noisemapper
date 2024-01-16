6741f01a3b40b0544aa05982b75c7df2c48e0527
floflo330 <83714707+floflo330@users.noreply.github.com>
2024-01-16 11:33:23 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,27 +9,27 @@
     </div>
 </footer>
 -->
-
 <footer class="footer">
             <!-- Logo de l'entreprise à gauche -->
             <div class="flex-footer-item">
-                <img class="footer-logo"src="./RESSOURCES/IMAGES/logo_white.svg" alt="NoiseMapper">NoiseMapper
+                <img class="footer-logo"src="data/logo_white.svg" alt="NoiseMapper">NoiseMapper
             </div>
 
             <!-- Liens du plan du site à gauche -->
             <div class="flex-footer-item">
-                <a id="footer" href="./index.php">Accueil</a>
-                <a href="./tarifs.php">Tarifs</a>
-                <a href="./index.php">A propos</a>
-                <a href="./index.php">Mentions Légales</a>
+                <a id="footer" href="/">Accueil</a>
+                <a href="contact.php">Contact</a>
+                <a href="/">A propos</a>
+                <a href="mentions.php">Mentions Légales</a>
+                <a href="cgu.php">CGU</a>
                 <br>
-                © 2023 NoiseMapper
+                © 2024 NoiseMapper
             </div>
 
             <!-- Liens des réseaux sociaux à droite -->
             <div class="flex-footer-row">
-                <a href="#"><img class="social-icon" src="./RESSOURCES/IMAGES/facebook.svg" alt="Facebook"></a>
-                <a href="#"><img class="social-icon-small" src="./RESSOURCES/IMAGES/twitter.svg" alt="Twitter"></a>
-                <a href="#"><img class="social-icon-big" src="./RESSOURCES/IMAGES/instagram.svg" alt="Instagram"></a>
+                <a href="#"><img class="social-icon" src="data/facebook.svg" alt="Facebook"></a>
+                <a href="#"><img class="social-icon-small" src="data/twitter.svg" alt="Twitter"></a>
+                <a href="#"><img class="social-icon-big" src="data/instagram.svg" alt="Instagram"></a>
             </div>
 </footer>
