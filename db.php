58f067365d9324191f2fae08e7ae096794f98b46
AntoineDbn <156205631+AntoineDbn@users.noreply.github.com>
2024-01-16 20:09:36 +0100

    Update db.php

--- db.php
+++ db.php
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("La connexion a échoué : " . $conn->connect_error);
+} else {
+    echo "Connexion réussie à la base de données";
 }
 ?>
