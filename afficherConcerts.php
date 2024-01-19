8f288fdc1231ee4acfcf84137884cc781db468e9
AntoineDbn <156205631+AntoineDbn@users.noreply.github.com>
2024-01-19 10:17:11 +0100

    Create afficherConcerts.php

---
+++ afficherConcerts.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["selectConcert"])) {
+    // Récupérer la date sélectionnée depuis la requête
+    $selectedDate = $_POST["selectConcert"];
+
+    // Inclure le fichier de configuration de la base de données
+    include('./db.php');
+
+    // Requête pour récupérer les concerts associés à la date sélectionnée
+    $sql = "SELECT * FROM concert WHERE Date = '$selectedDate'";
+    $result = $conn->query($sql);
+
+    // Construire le HTML pour le tableau de concerts
+    $concertHTML = "<table border='1'>
+                    <tr>
+                        <th>Nom du concert</th>
+                        <th>Description</th>
+                        <!-- Ajoutez d'autres colonnes au besoin -->
+                    </tr>";
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $concertHTML .= "<tr>";
+            $concertHTML .= "<td>" . $row["Nom"] . "</td>";
+            $concertHTML .= "<td>" . $row["Description"] . "</td>";
+            // Ajoutez d'autres colonnes au besoin
+            $concertHTML .= "</tr>";
+        }
+    } else {
+        $concertHTML .= "<tr><td colspan='2'>Aucun concert disponible pour la date sélectionnée.</td></tr>";
+    }
+
+    $concertHTML .= "</table>";
+
+    // Fermer la connexion
+    $conn->close();
+
+    // Renvoyer le HTML du tableau de concerts
+    echo $concertHTML;
+}
+?>
